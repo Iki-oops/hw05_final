6515e949e0dab8a987432b11add87cf98236b07a
Iki <bambagaevdmitrij@gmail.com>
2021-03-29 21:57:14 +0800

    i

--- posts/views.py
+++ posts/views.py
@@ -29,7 +29,7 @@ def group_posts(request, slug):
 def profile(request, username):
     profile = get_object_or_404(User, username=username)
     profile_posts = profile.posts.all()
-    # paginator = Paginator(profile_posts, TEN_POSTS)
+    paginator = Paginator(profile_posts, TEN_POSTS)
     page_number = request.GET.get('page')
     page = paginator.get_page(page_number)
     following = profile.following.filter(user__username=request.user)
